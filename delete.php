fbec24edcd659057f57d739a0c1b8381ec793cb8
HammadJawedRauf <hammadjawed267@gmail.com>
2026-08-24 14:59:10 -0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,49 +1,15 @@
 <?php
 if ($_SERVER ["REQUEST_METHOD"] == "POST"){
-if(isset($_POST['register'])){
-?>
-<table border="1">
 
-    <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Roll No</th>
-        <th>Department</th>
-        <th>Action</th>
-    </tr>
+if(isset($_POST['deletebtn'])){
+$id=$_POST['STD_ID'];
+$sql = "DELETE FROM student WHERE STD_ID = :id";
 
-    <?php foreach ($students as $student) { ?>
+$stmt = $con->prepare($sql);
 
-       <div></div> <tr>
+$stmt->execute([
+    ':id' => $id
+]);
 
-            <td><?php echo $student['STD_ID']; ?></td>
-
-            <td><?php echo $student['NAME']; ?></td>
-
-            <td><?php echo $student['ROLLNO']; ?></td>
-
-            <td><?php echo $student['DEPARTMENT']; ?></td>
-
-            <td>
-
-                <form method="POST">
-
-                    <input
-                        type="hidden"
-                        name="delete"
-                        value="<?php echo $student['STD_ID']; ?>"
-                    >
-
-                    <button type="submit" name="deleteBtn">
-                        Delete
-                    </button>
-
-                </form>
-
-            </td>
-
-        </tr>
-</div>
-    <?php }}} ?>
-
-</table>
+echo "<br>Student deleted successfully!";
+}}?>
